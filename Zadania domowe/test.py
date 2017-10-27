cd8b745146d1953530dbfeda30fab86c6cd45a7f
blue-mica <bluemicam3@gmail.com>
2017-10-27 02:10:43 +0200

    adding files
    calculator i testy.py
    empty file cmd_Win.py

---
+++ Zadania domowe/test.py
@@ -0,0 +1,14 @@
+
+#oryginal dodawal 2 liczby; dodalem 3
+
+# Store input numbers
+print('We are going to add 3 numbers, so please:')
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+num3 = input('Enter third number: ')
+
+# Add three numbers
+sum = float(num1) + float(num2) + float(num3)
+
+# Display the sum
+print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2, num3, sum))
